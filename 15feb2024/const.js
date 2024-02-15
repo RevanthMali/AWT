0a6bcef07c17adc6737a0b89bf62e8a919ea2017
RevanthMali <malirevanth06@gmail.com>
2024-02-15 09:33:39 +0530

    added the programs on 15feb

---
+++ 15feb2024/const.js
@@ -0,0 +1,8 @@
+
+// This const is same as 'let' its scope is 
+const a = 10 ;
+{
+    const a = 4;
+    console.log(a);
+} 
+console.log(a);
---
+++ 15feb2024/var.js
@@ -0,0 +1,22 @@
+// var x = 3;
+// {
+//     var x = 4;
+//     console.log(x);  
+// } 
+// console.log(x) 
+
+
+var x = 5;
+{
+    let x = 10;
+    console.log(x)
+} 
+console.log(x) 
+
+// this is not allowed in javascript
+
+// let x = 6 ;    
+//  {   var x = 8 ; 
+//     console.log(x)
+//  } 
+//  console.log(x)
