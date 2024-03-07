1612cff0dd81f19b478a810de726489c4af9373b
RevanthMali <malirevanth06@gmail.com>
2024-03-07 11:09:25 +0530

    configure firebase

---
+++ 7mar2024_firebase/src/index.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+ 
+const firebaseConfig = {
+    apiKey: "key",
+    authDomain: "myfirstapp-a9554.firebaseapp.com",
+    projectId: "myfirstapp-a9554",
+    storageBucket: "id",
+    messagingSenderId: "id",
+    appId: "id"
+  };
+
+const app = initializeApp(firebaseConfig);
+const db =  firebase.firestore()
+
+try {
+  db.collection("users").add({
+    doc_id:"11111",
+    name:"Revanth",
+    roll_no:"1238"
+});
+  console.log(`Document written with ID:  ${doc_id}`);
+} catch (e) {
+  console.error("Error adding document: ", e);
+}
